5abb4fe462d757c46ec958514780adc9d1d88504
王佳 <wangjia_1919@163.com>
2019-06-24 09:38:12 +0800

    first blood

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,13 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/attributes-order': 'warning',
+    'vue/no-confusing-v-for-v-if': 'warning',
+    'vue/order-in-components': 'warning',
+    'vue/this-in-template': 'warning',
+    'vue/require-default-prop': 'warning',
+    'vue/require-prop-types': 'warning'
   },
   parserOptions: {
     parser: 'babel-eslint'
