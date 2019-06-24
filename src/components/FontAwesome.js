2f0ccc356f171ef2037fc7a7d4c226d0505ac5ec
王佳 <wangjia_1919@163.com>
2019-06-24 15:06:21 +0800

    fix lint

--- src/components/FontAwesome.js
+++ src/components/FontAwesome.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 
-import {FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText} from '@fortawesome/vue-fontawesome'
-import {library} from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-import {fas} from '@fortawesome/free-solid-svg-icons'
-import {fab} from '@fortawesome/free-brands-svg-icons'
-import {far} from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 
 // 使用的组件
 Vue.component('font-awesome-icon', FontAwesomeIcon)
